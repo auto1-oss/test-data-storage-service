001630cf872fdb430a1001da257aebafd5e9654a
vbarduk <35689570+vbarduk@users.noreply.github.com>
2022-09-26 15:05:02 +0200

    SPIKE refactor test configs (#7)
    * SPIKE refactor test configs
    
    * SPIKE refactor test configs
    
    * SPIKE rename properties
    
    * SPIKE magix fix
    
    Co-authored-by: dp1auto <dmitrii.popov@auto1.com>

--- src/test/java/com/auto1/testdatastorage/TestDataStorageApplicationTests.java
+++ src/test/java/com/auto1/testdatastorage/TestDataStorageApplicationTests.java
@@ -2,8 +2,10 @@ package com.auto1.testdatastorage;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TestDataStorageApplicationTests {
 
 	@Test
