0c08afa2074682b855feb6e5de333ea3598a8c55
xsmirnovx <alexandr.smirnov@auto1.com>
2023-01-09 19:07:13 +0100

    cleanup

--- src/test/java/com/auto1/testdatastorage/OmniTypeTest.java
+++ src/test/java/com/auto1/testdatastorage/OmniTypeTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -26,7 +25,6 @@ import static org.hamcrest.Matchers.is;
 @SpringBootTest(classes = TestDataStorageApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
-@AutoConfigureMockMvc
 public class OmniTypeTest {
 
     @Autowired
