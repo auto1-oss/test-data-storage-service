267d6c12558f5b1bfc3fa0d1405d4eac523e2b35
sun <alex.sun@auto1.com>
2025-04-17 13:46:38 +0200

    Add license headers

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,12 @@
+#
+# This file is part of the auto1-oss/test-data-storage-service.
+#
+# (c) AUTO1 Group SE https://www.auto1-group.com
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+
 FROM adoptopenjdk:11-jre-hotspot
 ADD target/*.jar application.jar
 ENTRYPOINT [ "sh", "-c", "java $JAVA_OPTS -jar /application.jar" ]
--- src/main/resources/db/migration/V2__add_omni_queue_index.sql
+++ src/main/resources/db/migration/V2__add_omni_queue_index.sql
@@ -1,2 +1,11 @@
+--
+-- This file is part of the auto1-oss/test-data-storage-service.
+--
+-- (c) AUTO1 Group SE https://www.auto1-group.com
+--
+-- For the full copyright and license information, please view the LICENSE
+-- file that was distributed with this source code.
+--
+
 CREATE INDEX IF NOT EXISTS idx_omni_queue_omni_type_id_archived_id
     ON test_data_storage.omni_queue (omni_type_id, archived, id);
